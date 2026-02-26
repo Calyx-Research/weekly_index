31e95a4f17aa2a5699ab4662887c8c2e1171808d
Ahmad Babatunde <ahmadtijani1639@gmail.com>
2026-02-26 16:10:14 +0100

    manual run test

--- weekly_auto.py
+++ weekly_auto.py
@@ -20,9 +20,9 @@ import os
 # =========================
 load_dotenv() # This loads the variables from .env
 
-USER = os.getenv("USER")
+DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-HOST = os.getenv("HOST")
+DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
